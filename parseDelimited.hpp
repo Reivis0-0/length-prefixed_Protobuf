2bd859a4a2c569f033f2f932883e558a754643e8
Revis0 <anna.sevvost@gmail.com>
2025-08-05 23:09:21 +0300

    Removed extra files

--- parseDelimited.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PARSEDELIMITED_HPP
-#define PARSEDELIMITED_HPP
-
-#include<memory>
-
-template<typename Message>
-std::shared_ptr<Message> parseDelimited(const void* data, size_t size, size_t* bytesConsumed = 0); 
-
-#endif
