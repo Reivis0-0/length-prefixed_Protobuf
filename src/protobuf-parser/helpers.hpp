7894e2237e2a1b2e6c116c5d4337691a904d9df5
Anna Sevostyanova <163858998+Reivis0@users.noreply.github.com>
2025-08-13 14:39:37 +0300

    Update helpers.hpp

--- src/protobuf-parser/helpers.hpp
+++ src/protobuf-parser/helpers.hpp
@@ -5,24 +5,27 @@
 #include <vector>
 #include <memory>
 
-typedef std::vector<char> Data;
-typedef std::shared_ptr<const Data> PointerToConstData;
-typedef std::shared_ptr<Data> PointerToData;
-
 #if GOOGLE_PROTOBUF_VERSION >= 3012004
 #define PROTOBUF_MESSAGE_BYTE_SIZE(message) ((message).ByteSizeLong())
 #else
 #define PROTOBUF_MESSAGE_BYTE_SIZE(message) ((message).ByteSize())
 #endif
 
+typedef std::vector<char> Data;
+typedef std::shared_ptr<const Data> PointerToConstData;
+typedef std::shared_ptr<Data> PointerToData;
+
 template <typename Message>
 PointerToConstData serializeDelimited(const Message& msg) {
     const size_t messageSize = PROTOBUF_MESSAGE_BYTE_SIZE(msg);
     const size_t headerSize = google::protobuf::io::CodedOutputStream::VarintSize32(messageSize);
+    
     const PointerToData& result = std::make_shared<Data>(headerSize + messageSize);
     google::protobuf::uint8* buffer = reinterpret_cast<google::protobuf::uint8*>(&*result->begin());
+    
     google::protobuf::io::CodedOutputStream::WriteVarint32ToArray(messageSize, buffer);
     msg.SerializeWithCachedSizesToArray(buffer + headerSize);
+    
     return result;
 }
 
