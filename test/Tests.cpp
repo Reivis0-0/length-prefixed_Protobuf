c274b5032ab58586fe6ac714ae4291208b045852
Anna Sevostyanova <163858998+Reivis0@users.noreply.github.com>
2025-08-13 14:11:08 +0300

    Update Tests.cpp 2.0

--- test/Tests.cpp
+++ test/Tests.cpp
@@ -3,9 +3,92 @@
 
 #include <gtest/gtest.h>
 
+//Testing function parseDelimited
+TEST(MyTestParserDelimeted, FullMessage)
+{
+  TestTask::Messages::WrapperMessage m;
+  TestTask::Messages::FastResponse* fast = m.mutable_fast_response();
+  std::string s = "20250811T100000.00";
+  fast->set_current_date_time(s);
+
+  TestTask::Messages::SlowResponse* slow = m.mutable_slow_response();
+  slow->set_connected_client_count(209);
+
+  TestTask::Messages::RequestForSlowResponse* slow_request = m.mutable_request_for_slow_response();
+  slow_request->set_time_in_seconds_to_sleep(10);
+
+  TestTask::Messages::RequestForFastResponse* fast_request = m.mutable_request_for_fast_response();  
+
+  auto data = serializeDelimited(m);
+  std::shared_ptr<TestTask::Messages::WrapperMessage> result;
+  size_t bytesConsumed = 0;
+
+  result = parseDelimited<TestTask::Messages::WrapperMessage>(data->data(), data->size(), &bytesConsumed);
+  ASSERT_FALSE(result == nullptr);
+  ASSERT_TRUE(result->has_request_for_fast_response());
+  ASSERT_TRUE(result->has_fast_response());
+  ASSERT_TRUE(result->has_slow_response());
+  ASSERT_TRUE(result->has_request_for_slow_response());
+  ASSERT_TRUE(result->fast_response().has_current_date_time());
+  ASSERT_TRUE(result->slow_response().has_connected_client_count());
+  ASSERT_TRUE(result->request_for_slow_response().has_time_in_seconds_to_sleep());
+}
+
+TEST(MyTestParserDelimeted, NonExistentMessage)
+{
+  std::string data = "\x09wronggggg";
+  EXPECT_THROW(parseDelimited<TestTask::Messages::WrapperMessage>(data.data(), data.size()), std::runtime_error);
+}
+
+TEST(MyTestParserDelimeted, NullMessage)
+{
+  std::shared_ptr<TestTask::Messages::WrapperMessage> result;
+  std::string data = "";
+  size_t bytesConsumed = 0;
+
+  result = parseDelimited<TestTask::Messages::WrapperMessage>(data.data(), data.size(), &bytesConsumed);
+  ASSERT_TRUE(result == nullptr);
+  EXPECT_EQ(bytesConsumed, 0);
+}
+
+TEST(MyTestParserDelimeted, DamageMessage)
+{
+  TestTask::Messages::WrapperMessage m;
+  TestTask::Messages::SlowResponse* slow = m.mutable_slow_response();
+  slow->set_connected_client_count(9);
+
+  auto data = serializeDelimited(m);
+  std::string data_str = std::string(data->begin(), data->end());
+
+  //Dammaging message
+  data_str[data_str.length() - 2] = 'a';
+
+  EXPECT_THROW(parseDelimited<TestTask::Messages::WrapperMessage>(data_str.data(), data_str.size()), std::runtime_error);
+}
+
+TEST(MyTestParserDelimeted, WrongLengthPrefixedByte)
+{
+  TestTask::Messages::WrapperMessage m;
+  TestTask::Messages::FastResponse* fast = m.mutable_fast_response();
+  std::string s = "20250811T100000.00";
+  fast->set_current_date_time(s);
+
+  auto data = serializeDelimited(m);
+  std::string data_str = std::string(data->begin(), data->end());
+  data_str[0] += 10;
+
+  std::shared_ptr<TestTask::Messages::WrapperMessage> result;
+  size_t bytesConsumed = 0;
+  result = parseDelimited<TestTask::Messages::WrapperMessage>(data_str.data(), data_str.size(), &bytesConsumed);
+
+  ASSERT_TRUE(result == nullptr);
+  EXPECT_EQ(bytesConsumed, 0);
+}
+
+
+//Testing class DelimitedMessagesStreamParser
 TEST(MyTestParser, FullMessage)
 {
-    //Create FullMessage
     TestTask::Messages::WrapperMessage m;
     TestTask::Messages::FastResponse* fast = m.mutable_fast_response();
     std::string s = "20250811T100000.00";
@@ -19,12 +102,10 @@ TEST(MyTestParser, FullMessage)
 
     TestTask::Messages::RequestForFastResponse* fast_request = m.mutable_request_for_fast_response();
 
-    //FullMessage to string
     std::string data_str;
     auto data = serializeDelimited(m);
     data_str.append(data->begin(), data->end());
 
-    //Parsing
     typedef DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> Parser;
     Parser parser;
     std::list<std::shared_ptr<const TestTask::Messages::WrapperMessage>> parsedMessages = parser.parse(data_str);
@@ -42,7 +123,6 @@ TEST(MyTestParser, FullMessage)
 
 TEST(MyTestParser, SeveralFullMessages)
 {
- //Create FullMessage
   TestTask::Messages::WrapperMessage m;
   TestTask::Messages::FastResponse* fast = m.mutable_fast_response();
   std::string s = "20250810T100000.00";
@@ -56,7 +136,6 @@ TEST(MyTestParser, SeveralFullMessages)
 
   TestTask::Messages::RequestForFastResponse* fast_request = m.mutable_request_for_fast_response();
 
-  //FullMessage to string
   std::string data_str;
   auto data = serializeDelimited(m);
   data_str.append(data->begin(), data->end());
@@ -65,7 +144,6 @@ TEST(MyTestParser, SeveralFullMessages)
   std::list<std::shared_ptr<const TestTask::Messages::WrapperMessage>> parsedMessages;
   std::list<std::shared_ptr<const TestTask::Messages::WrapperMessage>> result;
   
-  //parsing 3 messages
   for(int i = 0; i < 3; ++i)
   {
     parsedMessages = parser.parse(data_str);
@@ -86,7 +164,6 @@ TEST(MyTestParser, SeveralFullMessages)
 
 TEST(MyTestParser,MessagesWithExtraBytesBetween)
 {
-  //create First Full Message
   TestTask::Messages::WrapperMessage m1;
   TestTask::Messages::FastResponse* fast = m1.mutable_fast_response();
   std::string s = "20250810T100000.00";
@@ -100,7 +177,6 @@ TEST(MyTestParser,MessagesWithExtraBytesBetween)
 
   TestTask::Messages::RequestForFastResponse* fast_request = m1.mutable_request_for_fast_response();  
   
-  //Create Second Message
   TestTask::Messages::WrapperMessage m2;
   fast = m2.mutable_fast_response();
   s = "20250810T100000.00";
@@ -111,15 +187,12 @@ TEST(MyTestParser,MessagesWithExtraBytesBetween)
 
  fast_request = m2.mutable_request_for_fast_response();
 
-  //Full Message to string
   std::string data_str;
   auto data = serializeDelimited(m1);
   data_str.append(data->begin(), data->end());
 
-  //Add extra bytes between messages
   data_str.append(std::string("9qwertyuio"));
 
-  //Second message to string
   data = serializeDelimited(m2);
   data_str.append(data->begin(), data->end());
   
@@ -127,7 +200,6 @@ TEST(MyTestParser,MessagesWithExtraBytesBetween)
   Parser parser;
   std::list<std::shared_ptr<const TestTask::Messages::WrapperMessage>> parsedMessages;
 
-  //Parsing
   parsedMessages = parser.parse(data_str);
   for(auto msg : parsedMessages)
   {
@@ -141,12 +213,10 @@ TEST(MyTestParser,MessagesWithExtraBytesBetween)
 
 TEST(MyTestParser, DamageMessage)
 {
-  //Create Full Message
   TestTask::Messages::WrapperMessage m;
   TestTask::Messages::SlowResponse* slow = m.mutable_slow_response();
   slow->set_connected_client_count(9);
 
-  //Full Message to string
   std::string data_str;
   auto data = serializeDelimited(m);
   data_str.append(data->begin(), data->end()); 
@@ -156,7 +226,6 @@ TEST(MyTestParser, DamageMessage)
   data_str[data_str.length() - 2] = 'b';
   data_str[data_str.length() - 3] = 'c';
 
-  //Parsing 
   typedef DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> Parser;
   Parser parser;
   EXPECT_THROW(parser.parse(data_str), std::runtime_error);
@@ -164,108 +233,19 @@ TEST(MyTestParser, DamageMessage)
 
 TEST(MyTestParser, ExtraBytesBeforeAndAfterMessage)
 {
-    //Create Full Message
   TestTask::Messages::WrapperMessage m;
   TestTask::Messages::SlowResponse* slow = m.mutable_slow_response();
   slow->set_connected_client_count(9);
 
-  //Full Message to string
   std::string data_str;
   auto data = serializeDelimited(m);
   data_str.append(std::string(2, '3'));
   data_str.append(data->begin(), data->end());
   data_str.append(std::string(2, '2'));
 
-  //Parsing
   typedef DelimitedMessagesStreamParser<TestTask::Messages::WrapperMessage> Parser;
   Parser parser;
   std::list<std::shared_ptr<const TestTask::Messages::WrapperMessage>> parsedMessages;
   parsedMessages = parser.parse(data_str);
   EXPECT_EQ(parsedMessages.size(), 0);
-}
-
-TEST(MyTestParserDelimeted, FullMessage)
-{
-  //Create Full Message
-  TestTask::Messages::WrapperMessage m;
-  TestTask::Messages::FastResponse* fast = m.mutable_fast_response();
-  std::string s = "20250811T100000.00";
-  fast->set_current_date_time(s);
-
-  TestTask::Messages::SlowResponse* slow = m.mutable_slow_response();
-  slow->set_connected_client_count(209);
-
-  TestTask::Messages::RequestForSlowResponse* slow_request = m.mutable_request_for_slow_response();
-  slow_request->set_time_in_seconds_to_sleep(10);
-
-  TestTask::Messages::RequestForFastResponse* fast_request = m.mutable_request_for_fast_response();  
-  
-  //Parsing 
-  auto data = serializeDelimited(m);
-  std::shared_ptr<TestTask::Messages::WrapperMessage> result;
-  size_t bytesConsumed = 0;
-
-  result = parseDelimited<TestTask::Messages::WrapperMessage>(data->data(), data->size(), &bytesConsumed);
-  ASSERT_FALSE(result == nullptr);
-  ASSERT_TRUE(result->has_request_for_fast_response());
-  ASSERT_TRUE(result->has_fast_response());
-  ASSERT_TRUE(result->has_slow_response());
-  ASSERT_TRUE(result->has_request_for_slow_response());
-  ASSERT_TRUE(result->fast_response().has_current_date_time());
-  ASSERT_TRUE(result->slow_response().has_connected_client_count());
-  ASSERT_TRUE(result->request_for_slow_response().has_time_in_seconds_to_sleep());
-}
-
-TEST(MyTestParserDelimeted, NonExistentMessage)
-{
-  std::string data = "\x05wrong";
-  EXPECT_THROW(parseDelimited<TestTask::Messages::WrapperMessage>(data.data(), data.size()), std::runtime_error);
-}
-
-TEST(MyTestParserDelimeted, NullMessage)
-{
-  std::shared_ptr<TestTask::Messages::WrapperMessage> result;
-  std::string data = "";
-  size_t bytesConsumed = 0;
-
-  result = parseDelimited<TestTask::Messages::WrapperMessage>(data.data(), data.size(), &bytesConsumed);
-  ASSERT_TRUE(result == nullptr);
-  EXPECT_EQ(bytesConsumed, 0);
-}
-
-TEST(MyTestParserDelimeted, DamageMessage)
-{
-  //Create Message
-  TestTask::Messages::WrapperMessage m;
-  TestTask::Messages::SlowResponse* slow = m.mutable_slow_response();
-  slow->set_connected_client_count(9);
-
-  auto data = serializeDelimited(m);
-  std::string data_str = std::string(data->begin(), data->end());
-
-  //Dammaging message
-  data_str[data_str.length() - 2] = 'a';
-
-  EXPECT_THROW(parseDelimited<TestTask::Messages::WrapperMessage>(data_str.data(), data_str.size()), std::runtime_error);
-}
-
-TEST(MyTestParserDelimeted, WrongLengthPrefixedByte)
-{
-  //Create Message
-  TestTask::Messages::WrapperMessage m;
-  TestTask::Messages::FastResponse* fast = m.mutable_fast_response();
-  std::string s = "20250811T100000.00";
-  fast->set_current_date_time(s);
-
-  auto data = serializeDelimited(m);
-  std::string data_str = std::string(data->begin(), data->end());
-  data_str[0] += 10;
-
-  //Parsing
-  std::shared_ptr<TestTask::Messages::WrapperMessage> result;
-  size_t bytesConsumed = 0;
-  result = parseDelimited<TestTask::Messages::WrapperMessage>(data_str.data(), data_str.size(), &bytesConsumed);
-
-  ASSERT_TRUE(result == nullptr);
-  EXPECT_EQ(bytesConsumed, 0);
 }
